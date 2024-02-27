6487890741428b64136012512c6bc2e51a2fad85
K1M4K-ID <46388169+K1M4K-ID@users.noreply.github.com>
2024-02-27 10:21:23 -0500

    Add files via upload

---
+++ fix-zipalign.sh
@@ -0,0 +1,53 @@
+# !/bin/bash
+# fix zipalign made by K1M4K-ID
+#
+# clear
+clear
+sleep 1
+echo -e "[+] checking essential tools"
+sleep 1
+if command -v "figlet" &> /dev/null;
+then
+	echo ""
+	sleep 1
+	clear
+else
+	echo "[!]"
+fi
+
+figlet zipalign
+echo
+architecture=$(uname -m)
+
+if [ $architecture = "aarch64" ];
+then
+	echo "[+] install zipalign"
+	echo
+	sleep 1
+	apt-get install android-libandroidfw -y &> /dev/null
+	wget http://ftp.de.debian.org/debian/pool/main/a/android-platform-build/zipalign_8.1.0+r23-2_arm64.deb &> /dev/null
+	dpkg -i zipalign_8.1.0+r23-2_arm64.deb &> /dev/null
+
+elif [ $architecture = "amd64" ];
+then
+	echo "[+] install zipalign"
+        sleep 1
+	apt-get install android-libandroidfw -y &> /dev/null
+	wget http://ftp.de.debian.org/debian/pool/main/a/android-platform-build/zipalign_8.1.0+r23-2_amd64.deb &> /dev/null
+	dpkg -i zipalign_8.1.0+r23-2_amd64.deb &> /dev/null
+fi
+
+desktop="zipalign_8.1.0+r23-2_amd64.deb"
+mobile="zipalign_8.1.0+r23-2_arm64.deb"
+
+# Memeriksa apakah file ada
+if [ -f "$mobile" ]; then
+	rm -f "$mobile"
+elif [ -f "$desktop" ]; then
+	rm -f "$desktop"
+else
+	echo "File tidak ditemukan"
+fi
+        echo "[-] typing: zipalign"
+        sleep 1
+	echo
